57f29438b94537160cf1776e3a972ff1fd40b001
Suchan An <dobestan@gmail.com>
2016-03-28 15:36:21 +0900

    init Comment model: Comment has a content

---
+++ fastagram/posts/models/comment.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+    )
+    post = models.ForeignKey(
+        "Post",
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True, )
+    updated_at = models.DateTimeField(auto_now=True, )
