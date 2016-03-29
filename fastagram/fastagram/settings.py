31ae4f4a166794ece662a76c5bdeff0a8fc98244
Suchan An <dobestan@gmail.com>
2016-03-29 15:21:32 +0900

    update MEDIA settings

--- fastagram/fastagram/settings.py
+++ fastagram/fastagram/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIR, "dist", "media")
 
 
 # Custom Authentication Settings
--- fastagram/fastagram/urls.py
+++ fastagram/fastagram/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 from fastagram.views import *
 from users.views import *
@@ -14,4 +16,4 @@ urlpatterns = [
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^signup/$', SignupView.as_view(), name='signup'),
     url(r'^(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
