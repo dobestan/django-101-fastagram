d38638366470ee80bdbe5fc6ce03d06a08159ea8
Suchan An <dobestan@gmail.com>
2016-03-31 16:38:29 +0900

    update settings: add debug_toolbar, django_extensions

--- fastagram/fastagram/settings.py
+++ fastagram/fastagram/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+    'debug_toolbar',
+
     'fastagram',
     'users',
     'posts',
