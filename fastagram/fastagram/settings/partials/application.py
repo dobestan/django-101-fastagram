d879b7e809fe18f907a4f003de7d03db153ef16f
Suchan An <dobestan@gmail.com>
2016-03-31 17:18:58 +0900

    fix BASE_DIR, PROJECT_ROOT_DIR

--- fastagram/fastagram/settings/partials/application.py
+++ fastagram/fastagram/settings/partials/application.py
@@ -2,7 +2,15 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__)
+            )
+        )
+    )
+)
 PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
