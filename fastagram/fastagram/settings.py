5b9b04b2467cb656ed782e09f511902f71744f27
Suchan An <dobestan@gmail.com>
2016-03-31 15:48:23 +0900

    update django database settings: change default sqlite to psql

--- fastagram/fastagram/settings.py
+++ fastagram/fastagram/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'fastagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fastagram',
+        'USER': 'dobestan',
+        'PASSWORD': 'dkstncks',
     }
 }
 
