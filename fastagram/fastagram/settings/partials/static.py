0dc9be4d1be763a7fcf8b94bde0c16018226a9dc
Suchan An <dobestan@gmail.com>
2016-03-31 17:23:45 +0900

    init staticfiles: add css example

--- fastagram/fastagram/settings/partials/static.py
+++ fastagram/fastagram/settings/partials/static.py
@@ -10,4 +10,5 @@ from .application import PROJECT_ROOT_DIR
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, "dist", "static")
 MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIR, "dist", "media")
