0d57aebfac83bc5089e625e5ec8fc7d591cbe6df
Suchan An <dobestan@gmail.com>
2016-04-01 16:35:53 +0900

    update STATICFILES_STORAGE: upload static, media to S3, and load from CloudFront

--- fastagram/fastagram/settings/partials/application.py
+++ fastagram/fastagram/settings/partials/application.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'pipeline',
+    'storages',
 
     'fastagram',
     'users',
--- fastagram/fastagram/settings/production.py
+++ fastagram/fastagram/settings/production.py
@@ -1,3 +1,5 @@
+import os
+
 import raven
 
 from .partials import *
@@ -18,3 +20,16 @@ INSTALLED_APPS += [
 RAVEN_CONFIG = {
     'dsn': 'https://4f2bb88d49f345e88f63bb8066cdccb9:3d6fe481eee24ec58186509440d6a7ff@app.getsentry.com/72479',
 }
+
+
+STATICFILES_STORAGE = 'fastagram.storage.S3PipelineCachedStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+
+AWS_S3_CUSTOM_DOMAIN = 'd643ns7eahkdr.cloudfront.net'
+AWS_S3_URL_PROTOCOL = 'https'
+
+STATIC_URL = "https://d643ns7eahkdr.cloudfront.net/"
